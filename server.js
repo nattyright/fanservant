22342ae9d997dd2b92f926e5e4755437369d77eb
Connie Hu <hichoshi@gmail.com>
2022-05-13 00:41:16 +0800

    CORS

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://profile.fatechan.top");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header({
+    "Access-Control-Allow-Origin": "https://profile.fatechan.top",
+    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
+  });
   next();
 });
 

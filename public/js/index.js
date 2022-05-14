e0ad9baac2d6893468dbc56f7cfd11b79ccbfb6e
Connie Hu <hichoshi@gmail.com>
2022-05-14 21:56:50 +0800

    index help ui update

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,17 @@
 
 $("#disqus_thread_toggle").on("click", function (e) {
     $("#disqus_thread").toggle();
+    $("#instructions").hide();
+    $("#disqus_thread_toggle").toggleClass("active");
+    $("#instructions_toggle").removeClass("active");
 });
 
+$("#instructions_toggle").on("click", function (e) {
+    $("#instructions").toggle();
+     $("#disqus_thread").hide();
+     $("#instructions_toggle").toggleClass("active");
+     $("#disqus_thread_toggle").removeClass("active");
+});
 
 
 

f5cd698ceb4b09e7c4a9a7fd82545e970d692f1f
nat <hichoshi@gmail.com>
2022-05-15 16:49:58 +0800

    change profile page's servant class values to all lowercase/no spaces

--- public/js/profile-page.js
+++ public/js/profile-page.js
@@ -86,7 +86,7 @@ function loadServantProfile(a) {
   document.getElementById('pf-cardURL').src = a.info.cardartURL;
   //document.getElementById('pf-cardURL').src = "assets/cardart/" + a.info.cardURL + ".png";
   document.getElementById('pf-charagraph-frame').src = "assets/charagraph/charagraph_servant_0" + a.info.servantRarity.toString() + ".png";
-  document.getElementById('pf-charagraph-class').src = "assets/charagraph/class" + a.info.servantClass.toLowerCase() + a.info.servantRarity.toString() + "_charagraph" + ".png";
+  document.getElementById('pf-charagraph-class').src = "assets/charagraph/class" + a.info.servantClass.replace(/\s/g, '').toLowerCase() + a.info.servantRarity.toString() + "_charagraph" + ".png";
   document.getElementById('pf-info-servantClass').innerHTML = a.info.servantClass;
   document.getElementById('pf-info-servantName').innerHTML = a.info.servantName;
 
